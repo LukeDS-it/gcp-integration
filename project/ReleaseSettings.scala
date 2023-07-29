ac513baa08e4404c17ef0b7aeaf910e6a8dbcd90
LukeDS-it <luca.mcgyver@gmail.com>
2023-07-29 17:36:36 +0200

    Fixing warnings

--- project/ReleaseSettings.scala
+++ project/ReleaseSettings.scala
@@ -1,20 +1,21 @@
 import com.typesafe.sbt.GitPlugin.autoImport.git
 import com.typesafe.sbt.SbtNativePackager.autoImport.packageName
+import com.typesafe.sbt.packager.docker.DockerAlias
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.*
-import sbt.*
+import sbt.{Def, *}
 import sbt.Keys.*
 import sbt.librarymanagement.*
 import sbtrelease.ReleasePlugin.autoImport.releaseCommitMessage
 
 object ReleaseSettings extends LibraryManagementSyntax {
 
-  lazy val settings = Seq(
+  lazy val settings: Seq[Def.Setting[?]] = Seq(
     Docker / packageName := sys.env.getOrElse("DOCKER_IMAGE_NAME", name.value),
     dockerAlias := DockerAlias(
       dockerRepository.value,
       dockerUsername.value,
       packageName.value,
-      Option(git.gitDescribedVersion.value.getOrElse((version in ThisBuild).value))
+      Option(git.gitDescribedVersion.value.getOrElse((ThisBuild / version).value))
     ),
     packagedArtifacts := Map.empty,
     publish := (Docker / publish).value,
@@ -22,6 +23,6 @@ object ReleaseSettings extends LibraryManagementSyntax {
     pomIncludeRepository := { _ => false },
     publishArtifact := false,
     publishTo := Some("releases" at "https://example.com/"), // Needs to be set by sbt-release plugin
-    releaseCommitMessage := s"Setting version to ${(version in ThisBuild).value} [ci-skip]"
+    releaseCommitMessage := s"Setting version to ${(ThisBuild / version).value} [ci-skip]"
   )
 }
