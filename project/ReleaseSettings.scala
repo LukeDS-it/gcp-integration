eb6e8d15f820f87b002d235371235f1cff44863b
LukeDS-it <luca.mcgyver@gmail.com>
2023-07-31 08:51:56 +0200

    #minor Calculating release version

--- project/ReleaseSettings.scala
+++ project/ReleaseSettings.scala
@@ -3,10 +3,11 @@ import com.typesafe.sbt.SbtNativePackager.autoImport.packageName
 import com.typesafe.sbt.packager.docker.DockerAlias
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.*
 import sbt.{Def, *}
-import sbt.Keys.*
+import sbt.Keys.{version, *}
 import sbt.librarymanagement.*
-import sbtrelease.ReleasePlugin.autoImport.{releaseCommitMessage, releaseVersionBump}
+import sbtrelease.ReleasePlugin.autoImport.*
 import sbtrelease.Version.Bump
+import sbtrelease.{Version, versionFormatError}
 
 object ReleaseSettings extends LibraryManagementSyntax {
 
@@ -25,10 +26,16 @@ object ReleaseSettings extends LibraryManagementSyntax {
     publishArtifact := false,
     publishTo := Some("releases" at "https://example.com/"), // Needs to be set by sbt-release plugin
     releaseCommitMessage := s"Setting version to ${(ThisBuild / version).value} [ci-skip]",
-    releaseVersionBump := getVersionBumpStrategy
+    releaseVersion := { ver => getReleaseVersion(ver) }
   )
 
-  private def getVersionBumpStrategy: Bump = sys.env.getOrElse("VERSION_BUMP", "patch") match {
+  private def getReleaseVersion(current: String): String = getReleaseType match {
+    case Bump.Major => Version(current).map(_.bump(Bump.Major).string).getOrElse(versionFormatError(current))
+    case Bump.Minor => Version(current).map(_.bump(Bump.Minor).string).getOrElse(versionFormatError(current))
+    case _          => Version(current).map(_.withoutQualifier.string).getOrElse(versionFormatError(current))
+  }
+
+  private def getReleaseType: Bump = sys.env.getOrElse("VERSION_BUMP", "patch") match {
     case "major" => Bump.Major
     case "minor" => Bump.Minor
     case "patch" => Bump.Bugfix
